502c044f99c61e0eca556963438482dc2cb07282
NicoOhR <nimoohayonr@gmail.com>
2024-07-14 23:38:26 -0500

    actix web

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{HttpResponse, HttpServer, web, App};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
-    HttpServer::new(|| App::new.route("/", web::get().to(HttpResponse::Ok()))) //creates workers based on cores on system
+    HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok))) //creates workers based on cores on system
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
