3dbc52cda4a22cf9f195c0142acfa2d45e2aa330
NicoOhR <nimoohayonr@gmail.com>
2024-07-19 20:09:48 -0500

    moved functions to server.rs

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,11 @@
 use std::net::SocketAddr;
-
-use hyper::{body::Bytes,
-    server::conn::http1,
-    service::service_fn,
-    {Request, Response, Method, StatusCode}
-};
-
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
-use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full};
+use hyper::{server::conn::http1,service::service_fn};
 
-async fn user_request(
-    req: Request<hyper::body::Incoming>
-) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error>{
-    match (req.method(), req.uri().path()) {
-        (&Method::GET , "/") => Ok(Response::new(full(
-            "Try POSTing Data to /user_request",
-        ))),
-        (&Method::POST, "/user_request") => {
-            Ok(Response::new(req.into_body().boxed()))
-        },
-        _ => {
-            let mut not_found = Response::new(empty());
-            *not_found.status_mut() = StatusCode::NOT_FOUND;
-            Ok(not_found)
-        }
-    }
-}
-fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
-    Full::new(chunk.into())
-        .map_err(|never| match never {})
-        .boxed()
-}
 
-fn empty() -> BoxBody<Bytes, hyper::Error> {
-    Empty::<Bytes>::new()
-        .map_err(|never| match never {})
-        .boxed()
-}
+mod server;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([127,0,0,1], 3000));
@@ -50,7 +18,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let io = TokioIo::new(stream);
 
         tokio::task::spawn(async move {
-            if let Err(err) = http1::Builder::new().serve_connection(io, service_fn(user_request)).await{
+            if let Err(err) = http1::Builder::new().serve_connection(io, service_fn(server::user_request)).await{
                 eprintln!("Error Serving Connection: {:?}", err);
             }
         });
---
+++ src/server.rs
@@ -0,0 +1,37 @@
+
+use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full};
+use hyper::{
+    body::Bytes,
+    {Request, Response, Method, StatusCode}
+};
+
+
+pub async fn user_request(
+    req: Request<hyper::body::Incoming>
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error>{
+    match (req.method(), req.uri().path()) {
+        (&Method::GET , "/") => Ok(Response::new(full(
+            "Try POSTing Data to /user_request",
+        ))),
+        (&Method::POST, "/user_request") => {
+            Ok(Response::new(req.into_body().boxed()))
+        },
+        _ => {
+            let mut not_found = Response::new(empty());
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+
